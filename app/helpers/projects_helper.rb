2e3c85ba292ab999ea8a1147bba74ba4a6f92188
btmccollum <btmcco8@gmail.com>
2018-12-04 10:27:29 -0600

    changed project total hours helper to allow argument, updated applicable views to reflect such

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,6 @@
 module ProjectsHelper
-    def project_total_hours
-        total = @project.billable_hours
+    def project_total_hours(p)
+        total = p.billable_hours
 
         if total >= 3600
             new_total = ((total / 60) / 60).round(2)
