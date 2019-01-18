06a2d95ee88486b1a098579886997f4e30eb917b
btmccollum <btmcco8@gmail.com>
2019-01-18 17:18:01 -0600

    adding event listeners for dashboard project status dropdown, added successful ajax get request to load statu queries from projects controller

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,8 +2,6 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 $(function() {
-    console.log('projects.js is loaded')
-
     addListeners()
 });
 
@@ -12,22 +10,38 @@ function addListeners() {
 	$('a.js-AllProjects').on('click', function (e) {
         e.preventDefault();
 		getProjects();
-	})
+    });
+
+    $('a.js-ActiveProjects').on('click', function (e) {
+        e.preventDefault();
+        const params = { "status": "active" }
+		getProjects(params);
+    });
+
+    $('a.js-CompletedProjects').on('click', function (e) {
+        e.preventDefault();
+        params = { "status": "completed" }
+		getProjects(params);
+	});
 }
 
-function getProjects() {
-    $.get('/dashboard.json')
-        .done(function(data) {
-            debugger;
-            console.log("the projects are: ", data)
-            data.forEach(function(project) {
-                const addedProject = new Project(project)
-                console.log(addedProject)
-                $('.js-Projects').prepend(addedProject.postHTML());
-            });
-            // let newProject = new Project(data[0])
-            // console.log(newProject);
+function getProjects(query) {
+    $.ajax({
+        url: '/dashboard',
+        type: "GET",
+        dataType: 'json',
+        data: query
+    }).done(function(data) {
+        $('.js-Projects').html("");
+        data.forEach(function(project) {
+            const addedProject = new Project(project);
+            $('.js-Projects').prepend(addedProject.postHTML());
+        });
     });
+}
+
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)    
 }
 
 class Project {
@@ -45,23 +59,23 @@ class Project {
 // return HTML entry for dashboard page
 Project.prototype.postHTML = function() {
     return (`
-        <li class="list-group-item"><strong>Title:</strong> <a href="/projects/${this.id}">${this.title}</a> | <strong>Total Time:</strong> 0.0 Seconds  | <strong>Last Update:</strong> ${this.updated_at} | <strong>Status:</strong> ${this.status}</li>
+        <li class="list-group-item"><strong>Title:</strong> <a href="/projects/${this.id}">${this.title}</a> | <strong>Total Time:</strong> 0.0 Seconds  | <strong>Last Update:</strong> ${this.updated_at} | <strong>Status:</strong> ${capitalizeFirstLetter(this.status)}</li>
     `)
 }
 
-Project.prototype.getTimeEntries = () => {
-    $.get(action, params)
-        .done(function(json){
-            //getting back a js object of the item just created
-            let $ul = $("#comment-list")
-            //clearing user submission from text area
-            $("#comment_content").val("");
-            //mimicking the rails list style
-            $ul.prepend(`<li class="list-group-item"><strong>${json['user']['username']}</strong> says: ${json['comment']['content']} | <strong>Posted at:</strong> Just a moment ago... <a data-confirm="Are you sure?" class="btn-sm btn-danger pull-right buttonJS" rel="nofollow" data-method="delete" href="/comments/${json['comment']['id']}">X</a></li>`)
-        }).fail(function(response){
-            console.log("Something went wrong.", response);
-    });
-}
+// Project.prototype.getTimeEntries = () => {
+//     $.get(action, params)
+//         .done(function(json){
+//             //getting back a js object of the item just created
+//             let $ul = $("#comment-list")
+//             //clearing user submission from text area
+//             $("#comment_content").val("");
+//             //mimicking the rails list style
+//             $ul.prepend(`<li class="list-group-item"><strong>${json['user']['username']}</strong> says: ${json['comment']['content']} | <strong>Posted at:</strong> Just a moment ago... <a data-confirm="Are you sure?" class="btn-sm btn-danger pull-right buttonJS" rel="nofollow" data-method="delete" href="/comments/${json['comment']['id']}">X</a></li>`)
+//         }).fail(function(response){
+//             console.log("Something went wrong.", response);
+//     });
+// }
 
 Project.prototype.getComments = () => {
     $("#new_comment").on("submit", function(e) {
